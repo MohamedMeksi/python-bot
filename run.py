64dafc87ead291ae5b4de9f22305fa887e1e264c
MohamedMeksi <miksimohamed@gmail.com>
2025-06-02 15:37:33 +0100

    first3

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import logging
 
 from app import create_app
 
-
 app = create_app()
 
 if __name__ == "__main__":
     logging.info("Flask app started")
+    print("---------------------------------------------------------")
+    print("Flask application is running and accessible at 0.0.0.0:8000")
+    print("---------------------------------------------------------")
     app.run(host="0.0.0.0", port=8000)
